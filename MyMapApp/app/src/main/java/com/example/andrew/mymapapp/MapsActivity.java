04554b1036b59b03c3f24d03fc346e6bc0bef1c5
Shamusa <nguyena8047@gmail.com>
2017-05-31 09:26:35 -0700

    5/31
    Followed on instructions

--- MyMapApp/app/src/main/java/com/example/andrew/mymapapp/MapsActivity.java
+++ MyMapApp/app/src/main/java/com/example/andrew/mymapapp/MapsActivity.java
@@ -1,11 +1,14 @@
 package com.example.andrew.mymapapp;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.icu.text.StringPrepParseException;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -40,7 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private boolean isGPSEnabled = false;
     private boolean isNetworkEnabled = false;
     private boolean canGetLocation = false;
-    private final static long MIN_TIME_IN_UPDATES = 1000*15*1;
+    private final static long MIN_TIME_IN_UPDATES = 1000 * 15 * 1;
     private final static long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
 
     @Override
@@ -63,6 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -80,37 +84,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        LatLng birth = new LatLng(32.7157,-117.1611);
+        LatLng birth = new LatLng(32.7157, -117.1611);
         mMap.addMarker(new MarkerOptions().position(birth).title("Marker in Place of Birth"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
     }
 
-    public void changeMapType(View v){
-        if(mMap.getMapType()==GoogleMap.MAP_TYPE_NORMAL){
+    public void changeMapType(View v) {
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
             mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        }
-        else if(mMap.getMapType()==GoogleMap.MAP_TYPE_HYBRID){
+        } else if (mMap.getMapType() == GoogleMap.MAP_TYPE_HYBRID) {
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
     }
 
-    public void pointsOfInterest(View v){
+    public void pointsOfInterest(View v) {
         searchBarText = (EditText) findViewById(R.id.searchText);
-        String loc=searchBarText.getText().toString().toLowerCase();
-        if(loc.equals("balboa park")){
-            LatLng balboa = new LatLng(32.730831,-117.142586);
+        String loc = searchBarText.getText().toString().toLowerCase();
+        if (loc.equals("balboa park")) {
+            LatLng balboa = new LatLng(32.730831, -117.142586);
             mMap.addMarker(new MarkerOptions().position(balboa).title("Balboa Park"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(balboa));
         }
-        if(loc.equals("airport")){
-            LatLng airport = new LatLng(32.7338006,-117.193303792);
+        if (loc.equals("airport")) {
+            LatLng airport = new LatLng(32.7338006, -117.193303792);
             mMap.addMarker(new MarkerOptions().position(airport).title("San Diego Airport"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(airport));
+        }
+        if (loc.equals("seaworld")) {
+            LatLng seaworld = new LatLng(32.7648, -117.2266);
+            mMap.addMarker(new MarkerOptions().position(seaworld).title("Seaworld"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(seaworld));
+        }
+        if (loc.equals("zoo")) {
+            LatLng zoo = new LatLng(32.7347483943, -117.150943196);
+            mMap.addMarker(new MarkerOptions().position(zoo).title("San Diego Zoo"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(zoo));
         }
 
     }
 
-    @Override
+   /* @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.d(TAG, "Location services connected.");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.GET_PERMISSIONS){
@@ -166,7 +179,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onLocationChanged(Location location) {
+
         handleNewLocation(location);
+
     }
 
     @Override
@@ -181,55 +196,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onResume() {
         super.onResume();
         mGoogleApiClient.connect();
-    }
+    }*/
 
-    public void getLocation(){
+    public void getLocation() {
 
-        try{
+        try {
             locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 
             //get GPS status
             isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            if(isGPSEnabled){
-                Log.d(TAG,"getLocation: GPS enabled");
+            if (isGPSEnabled) {
+                Log.d(TAG, "getLocation: GPS enabled");
             }
             isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            if(isNetworkEnabled){
-                Log.d(TAG,"getLocation: Network enabled");
+            if (isNetworkEnabled) {
+                Log.d(TAG, "getLocation: Network enabled");
             }
 
-            if(!isGPSEnabled && !isNetworkEnabled){
-                Log.d(TAG,"getLocation: No provider is enabled");
-            } else{
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Log.d(TAG, "getLocation: No provider is enabled");
+            } else {
 
                 this.canGetLocation = true;
-
-                if(isNetworkEnabled){
-                    Log.d(TAG,"getLocation: Network enabled - requesting location updates");
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                if (isNetworkEnabled) {
+                    Log.d(TAG, "getLocation: Network enabled - requesting location updates");
                     locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
                             MIN_TIME_IN_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES,
                             locationListenerNetwork);
 
-                    Log.d(TAG,"getLocation: Network update request successful");
+                    Log.d(TAG, "getLocation: Network update request successful");
                     Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
                 }
 
-                if(isGPSEnabled){
-                    Log.d(TAG,"getLocation: GPS enabled - requesting location updates");
+                if (isGPSEnabled) {
+                    Log.d(TAG, "getLocation: GPS enabled - requesting location updates");
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                             MIN_TIME_IN_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES,
                             locationListenerGPS);
 
-                    Log.d(TAG,"getLocation: GPS update request successful");
+                    Log.d(TAG, "getLocation: GPS update request successful");
                     Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
                 }
 
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             Log.d(TAG, "Caught exception in getLocation");
             e.printStackTrace();
         }
@@ -237,5 +261,119 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    android.location.LocationListener locationListenerGPS = new android.location.LocationListener() {
 
+        @Override
+        public void onLocationChanged(Location location) {
+            //output is Log.d and Toast that GPS is enabled
+            Log.d(TAG, "locationListenerGPS: onLocationChanged: enabled");
+            //Toast.makeText(this, "GPS is enabled", Toast.LENGTH_SHORT);
+
+            //Drop a marker on map - create a method called dropMarker
+            dropMarker(location);
+
+            //Remove the network location updates
+            //locationManager.removeUpdates(locationListenerNetwork);
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            //output is Log.d and Toast that GPS is enabled and working
+            Log.d(TAG, "locationListenerGPS: onLocationChanged: enabled");
+            //Toast.makeText(this, "GPS is enabled", Toast.LENGTH_SHORT);
+
+            //setup a switch statement to check the status input parameter
+            //case LocationProvider.AVAILABLE --> output message to Log.d and Toast
+//            if (status == LocationProvider.AVAILABLE) {
+//                Log.d(TAG, "locationListenerGPS: Location Provider is availabe");
+//                //Toast.makeText(this, "Location provider availabe", Toast.LENGTH_SHORT);
+//            }
+//            //case LocationProvider.OUT_OF_SERVICE --> request updates from NETWORK_PROVIDER
+//            //case LocationProvider.TEMPORARILY_UNAVAILABLE --> request updates from NETWORK_PROVIDER
+//            else if(status== LocationProvider.OUT_OF_SERVICE|| status == LocationProvider.TEMPORARILY_UNAVAILABLE){
+//                isNetworkEnabled=true;
+//                isGPSEnabled=false;
+//            }
+//            //case default --> request updates from NETWORK_PROVIDER
+//            else{
+//                isNetworkEnabled=true;
+//                isGPSEnabled=false;
+//            }
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider){
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider){
+
+        }
+    };
+
+    android.location.LocationListener locationListenerNetwork = new android.location.LocationListener(){
+
+        @Override
+        public void onLocationChanged(Location location){
+            //output is Log.d and Toast that Network is enabled
+            Log.d(TAG, "locationListenerNetwork: onLocationChanged: Networkenabled");
+            //Toast.makeText(this, "Network is enabled", Toast.LENGTH_SHORT);
+
+            //Drop a marker on map - create a method called dropMarker
+            dropMarker(location);
+
+            //Relaunch the network provider request (requestLocationUpdates (NETWORK_PROVIDER))
+            //isNetworkEnabled=true;
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras){
+            //output is Log.d and Toast that NETWORK is enabled and working
+            Log.d(TAG,"locationListenerNetwork: onStatusChanged: Network is enabled and working");
+            //Toast.makeText(this,"Network is enabled and working", Toast.LENGTH_SHORT);
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider){
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider){
+
+        }
+    };
+
+    public void dropMarker(Location location){
+        LatLng currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
 }
